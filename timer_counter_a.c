bd56a1408f456fc59b7deeea96e4e038b2d104a0
martin <martin.clemons@mac.com>
2020-03-03 14:37:49 +0100

    add Timer/Counter A utility and configuration functions

---
+++ timer_counter_a.c
@@ -0,0 +1,107 @@
+/*! \file
+ *  timer_counter_a.c
+ *  60345-100-digi-dac-firmware
+ *  Copyright (c) 2020 Martin Clemons
+ */
+#include "timer_counter_a.h"
+
+
+/*** Private Global Variables ------------------------------------------------*/
+/*! \privatesection */
+
+
+/*** Public Global Variables -------------------------------------------------*/
+/*! \publicsection */
+
+
+/*** Private Function Prototypes ---------------------------------------------*/
+/*! \privatesection */
+
+
+/*** Interrupt Service Routines (ISR) ----------------------------------------*/
+/*! \privatesection */
+
+
+/*** Private Functions -------------------------------------------------------*/
+/*! \privatesection */
+
+
+/*** Public Functions --------------------------------------------------------*/
+/*! \publicsection */
+
+/*! Directly set Timer/Counter A Period register. Bypass buffer registers and
+ * set `PER` registers directly. Not recommended when timer is running.
+ * @param period The value to set `PER` registers to.
+ */
+void timerCounterASetPeriod(uint16_t period)
+{
+    TCA0.SINGLE.PER = period;
+}
+
+/*! Directly set Timer/Counter A Compare registers. Bypass buffer registers and
+ * set `CMP0`, `CMP1`, and `CMP2` registers directly. Not recommended when timer is running.
+ * @param compare Array of 3 uint16_t values to set CMP registers to. `CMP0` is
+ * set to the first element in the array (compare[0]), `CMP1` is set to the second
+ * element in the array, and `CMP2` is set to the third element in the array.
+ */
+void timerCounterASetCompare(const uint16_t compare[3])
+{
+    TCA0.SINGLE.CMP0 = compare[0];
+    TCA0.SINGLE.CMP1 = compare[1];
+    TCA0.SINGLE.CMP2 = compare[2];
+}
+
+/*! Get the counter value for Timer/Counter A.
+ * @return The 16-bit counter value.
+ */
+uint16_t timerCounterAGetCounter(void)
+{
+    return TCA0.SINGLE.CNT;
+}
+
+/*! Configure Timer/Counter A. This function configures only the minimum common
+ * parameters between different applications for TCA. More specific functions
+ * can be used to configure more specialized features.
+ * @param config Pointer to `timerCounterAConfig_s` with basic configuration
+ * values.
+ */
+void timerCounterAConfig(const struct timerCounterAConfig_s *config)
+{
+    // set prescale/clock select in CTRLA
+    TCA0.SINGLE.CTRLA = config->prescale;
+    // set waveform mode in CTRLB
+    TCA0.SINGLE.CTRLB = config->waveformMode;
+}
+
+/*! Configure Timer/Counter A event action. The event action determines how TCA
+ * responds to an incoming event.
+ * @param action `timerCounterAEventAction_e` with the desired action.
+ */
+void timerCounterAConfigEventAction(const enum timerCounterAEventAction_e action)
+{
+    TCA0.SINGLE.EVCTRL = action;
+}
+
+/*! Enable Timer/Counter A event action. This function sets the `CNTEI` bit in
+ * the `EVCTRL` register.
+ */
+void timerCounterAEnableEventAction(void)
+{
+    TCA0.SINGLE.EVCTRL |= TCA_SINGLE_CNTEI_bm;
+}
+
+/*! Disable Timer/Counter A event action. This function clears the `CNTEI` bit in
+ * the `EVCTRL` register.
+ */
+void timerCounterADisableEventAction(void)
+{
+    TCA0.SINGLE.EVCTRL &= ~TCA_SINGLE_CNTEI_bm;
+}
+
+/*! Enable Timer/Counter A. This function sets the `ENABLE` bit in
+ * the `CTRLA` register.
+ */
+void timerCounterAEnable(void)
+{
+    TCA0.SINGLE.CTRLA |= TCA_SINGLE_ENABLE_bm;
+}
---
+++ timer_counter_a.h
@@ -0,0 +1,69 @@
+/*! \file
+ *  timer_counter_a.h
+ *  60345-100-digi-dac-firmware
+ *  Copyright (c) 2020 Martin Clemons
+ *
+ *  Enter file description here.
+ */
+#pragma once
+
+#include <inttypes.h>
+#include <stdbool.h>
+#include <avr/io.h>
+
+
+/*** Public Variables --------------------------------------------------------*/
+/*! \publicsection */
+
+/*! Timer/Counter A Pre-scale factor. TCA prescaler always runs from the peripheral clock.
+ */
+enum timerCounterAPrescale_e {
+    TCA_PRESCALE_DIV1               = TCA_SINGLE_CLKSEL_DIV1_gc,    ///< Prescale factor 1
+    TCA_PRESCALE_DIV2               = TCA_SINGLE_CLKSEL_DIV1_gc,    ///< Prescale factor 2
+    TCA_PRESCALE_DIV4               = TCA_SINGLE_CLKSEL_DIV1_gc,    ///< Prescale factor 4
+    TCA_PRESCALE_DIV8               = TCA_SINGLE_CLKSEL_DIV1_gc,    ///< Prescale factor 8
+    TCA_PRESCALE_DIV16              = TCA_SINGLE_CLKSEL_DIV1_gc,    ///< Prescale factor 16
+    TCA_PRESCALE_DIV64              = TCA_SINGLE_CLKSEL_DIV1_gc,    ///< Prescale factor 64
+    TCA_PRESCALE_DIV256             = TCA_SINGLE_CLKSEL_DIV1_gc,    ///< Prescale factor 256
+    TCA_PRESCALE_DIV1024            = TCA_SINGLE_CLKSEL_DIV1_gc,    ///< Prescale factor 1024
+};
+
+/*! Timer/Counter A Waveform Mode (normal, non-split)
+ */
+enum timerCounterAWaveformMode_e {
+    TCA_WAVEFORM_NORMAL             = TCA_SINGLE_WGMODE_NORMAL_gc,      ///< Normal mode
+    TCA_WAVEFORM_FREQUENCY          = TCA_SINGLE_WGMODE_FRQ_gc,         ///< Frequency mode
+    TCA_WAVEFORM_SINGLE_SLOPE       = TCA_SINGLE_WGMODE_SINGLESLOPE_gc, ///< Single-slope PWM
+    TCA_WAVEFORM_DUAL_SLOPE_TOP     = TCA_SINGLE_WGMODE_DSTOP_gc,       ///< Dual-slope PWM, OVF TOP
+    TCA_WAVEFORM_DUAL_SLOPE_BOTH    = TCA_SINGLE_WGMODE_DSBOTH_gc,      ///< Dual-slope PWM, OVF TOP and BOTTOM
+    TCA_WAVEFORM_DUAL_SLOPE_BOTTOM  = TCA_SINGLE_WGMODE_DSBOTTOM_gc,    ///< Dual-slope PWM, OVF BOTTOM
+};
+
+/*! Timer/Counter A Event Action options. Event input is only supported in non-split mode.
+ */
+
+enum timerCounterAEventAction_e {
+    TCA_EVENT_ACTION_POSITIVE_EDGE  = TCA_SINGLE_EVACT_POSEDGE_gc,  ///< Count on positive edge event
+    TCA_EVENT_ACTION_ANY_EDGE       = TCA_SINGLE_EVACT_ANYEDGE_gc,  ///< Count on any edge event
+    TCA_EVENT_ACTION_HIGH_LEVEL     = TCA_SINGLE_EVACT_HIGHLVL_gc,  ///< Count on prescaled clock while event line is high
+    TCA_EVENT_ACTION_DIRECTION      = TCA_SINGLE_EVACT_UPDOWN_gc,   ///< Count on prescaled clock. The Event controls the count direction. Up- counting when the event line is low, down-counting when the event line is high.
+};
+
+struct timerCounterAConfig_s {
+    enum timerCounterAPrescale_e prescale;
+    enum timerCounterAWaveformMode_e waveformMode;
+};
+
+
+
+/*** Public Functions --------------------------------------------------------*/
+/*! \publicsection */
+
+void timerCounterASetPeriod(uint16_t period);
+void timerCounterASetCompare(const uint16_t compare[3]);
+uint16_t timerCounterAGetCounter(void);
+void timerCounterAConfig(const struct timerCounterAConfig_s *config);
+void timerCounterAConfigEventAction(const enum timerCounterAEventAction_e action);
+void timerCounterAEnableEventAction(void);
+void timerCounterADisableEventAction(void);
+void timerCounterAEnable(void);
